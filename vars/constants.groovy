61254f00b195d0ec1dffba0769158977608018b2
rak134 <rakshithss@yahoo.com>
2025-02-02 12:02:22 +0530

    Updated constants.groovy

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -1,68 +1,14 @@
 import groovy.transform.Field
 
-// Constants defined with @Field to make them globally accessible
-@Field String GIT_URL = 'https://github.com/rak134/demo.git'
-@Field String PASSWORD = 'github-pass'
+@Field String AWS_ACCOUNT_ID = '120569619769'
+@Field String AWS_DEFAULT_REGION = 'ap-south-1'
+@Field String IMAGE_REPO_NAME = 'sample-webapp'
+@Field String GIT_REPO_URL = 'https://github.com/rak134/devops-automation.git'
+
+// Jenkins Credential IDs
+@Field String GIT_CREDENTIALS_ID = 'github-credentials'
+@Field String ECR_CREDENTIALS_ID = 'aws-ecr-credentials'
 
 def call() {
-    def mvnCommand = env.mvncommand ?: 'install'
-    def deployEnv = env.DeploymentEnvironment ?: 'staging'
-    
-    pipeline {
-        agent { label 'Slave' }
-        
-        stages {
-            stage("Checkout Code") {
-                steps {
-                    script {
-                        // Echoing the values for debugging purposes
-                        echo "This is the URL from shared library: ${GIT_URL}"
-                        echo "This is the password from shared library: ${PASSWORD}"
-                        
-                        // Checkout code using the GitSCM plugin
-                        checkout([
-                            $class: 'GitSCM',
-                            branches: [[name: '*/main']],
-                            userRemoteConfigs: [[
-                                url: GIT_URL,
-                                credentialsId: PASSWORD
-                            ]]
-                        ])
-                    }
-                }
-            }
-            
-            stage("Build") {
-                steps {
-                    script {
-                        sh "mvn clean ${mvnCommand}"
-                    }
-                }
-            }
-            
-            stage("Test") {
-                steps {
-                    sh 'mvn test'
-                }
-            }
-            
-            stage("Deploy To Artifactory") {
-                steps {
-                    configFileProvider([configFile(fileId: '8f023859-bc45-44cd-ae4a-e3462a3edcf3', variable: 'MAVEN_SETTINGS')]) {
-                        sh 'mvn deploy -s $MAVEN_SETTINGS'
-                    }
-                }
-            }
-            
-            stage("Deploy To Environment") {
-                steps {
-                    script {
-                        sh "echo 'I am Deploying this war or application to ${deployEnv}'"
-                    }
-                }
-            }
-        }
-    }
+    return this
 }
-
-return this
